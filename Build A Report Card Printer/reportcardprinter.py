5ec26090d3bca94122e921fc0f392ec2185f08d0
Dean McCrary <151476759+DeanMcCrary@users.noreply.github.com>
2026-06-29 21:28:26 -0400

    Added first completed project
    Added Basic Report Card Printer project from freeCodeCamp

---
+++ Build A Report Card Printer/reportcardprinter.py
@@ -0,0 +1,12 @@
+name = 'Dean'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 21
+print(age, type(age))
+
+score = 90.5
+print(isinstance(score, float))
+print(score, type(score))
